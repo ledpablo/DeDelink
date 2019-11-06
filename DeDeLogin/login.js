8eeb590fc3ec0ea48422c0e7c5e3f08f388940b2
AlexandruP28 <57462025+AlexandruP28@users.noreply.github.com>
2019-11-06 21:49:22 +0100

    Initial commit

---
+++ DeDeLogin/login.js
@@ -0,0 +1,10 @@
+function abreUrl(url,fulscrn){
+   /* window.open(url,'Ventana URL', 'width=550,height=255,top=152,left=32', 'fulscrn');*/
+    alert("Hacer Modal de Facebook y Google , Olvidar Contraseña");
+}
+
+
+function abreVentana(url,fulscrn){
+   /* window.open(url,'Ventana URL', 'width=550,height=255,top=152,left=32', 'fulscrn');*/
+    alert("Hacer Login te lleva al Inicio y Registrar a otra pagina");
+}
